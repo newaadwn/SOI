d72c555643942b665901eadebf4da37268a44050
newaadwn <itisnewdawn@gmail.com>
2025-08-18 21:03:00 +0900

    feat: Initialize project structure with main app, UI, and unit tests; add asset configurations

---
+++ ios/SOI/SOIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOIApp.swift
+//  SOI
+//
+//  Created by minchan Park on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct SOIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
